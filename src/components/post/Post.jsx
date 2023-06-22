83e69f3c931b267d75b87532815ae23342d4908a
Nazarii Sova <madrigalamar@gmail.com>
2023-06-22 15:02:50 +0300

    create centring for post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -6,7 +6,7 @@ const Post = () => {
     <div className="Post__container">
       <div className="Post__box">
         <div className="Post__title">dfgghfghf</div>
-        <div className="Post__description">dgfgdfgdfgdfgfdgdfg</div>
+        <div className="Post__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aliquid molestias facere quos ullam quia velit unde, numquam minus! Placeat iusto, eaque temporibus ratione amet veritatis iure perspiciatis eos velit vitae nostrum obcaecati omnis blanditiis impedit in quam mollitia deleniti rerum repellat repellendus id voluptates distinctio cupiditate. Minus, quasi enim?</div>
         <div className="Post__line-footer"></div>
       </div>
     </div>
